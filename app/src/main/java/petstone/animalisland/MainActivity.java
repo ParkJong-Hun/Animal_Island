b059d9111ccff63c7d45db7b2269053f46e009c4
text <thesn515@gmail.com>
2021-05-06 12:31:02 +0900

    내 브랜치 첫번째 커밋

--- app/src/main/java/petstone/animalisland/MainActivity.java
+++ app/src/main/java/petstone/animalisland/MainActivity.java
@@ -3,6 +3,7 @@ package petstone.animalisland;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "박종훈 브랜치.", Toast.LENGTH_SHORT).show();
     }
 }
