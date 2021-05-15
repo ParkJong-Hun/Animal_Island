f0c2aecf3f3d10114e752021c61d995cf0973283
ParkJong-Hun <thesn515@gmail.com>
2021-05-15 17:01:27 +0900

    2021/05/15 17:01 로그인 버튼 넘어가게 임시

--- app/src/main/java/petstone/project/animalisland/activity/LoginActivity.java
+++ app/src/main/java/petstone/project/animalisland/activity/LoginActivity.java
@@ -2,9 +2,12 @@ package petstone.project.animalisland.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import petstone.project.animalisland.R;
 
@@ -12,12 +15,25 @@ import petstone.project.animalisland.R;
 public class LoginActivity extends AppCompatActivity {
 
     //선언부
+    Button login_button;
 
     //화면 초기화
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        //바인딩
+        login_button = findViewById(R.id.login_button);
+        
+        //로그인 버튼
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
