ed343184a00604dd8353e38859362974b3e7f559
ParkJong-Hun <thesn515@gmail.com>
2021-05-20 13:29:41 +0900

    2021/05/20 13:29

--- app/src/main/java/petstone/project/animalisland/activity/MypageInfoEditActivity.java
+++ app/src/main/java/petstone/project/animalisland/activity/MypageInfoEditActivity.java
@@ -1,13 +1,16 @@
 package petstone.project.animalisland.activity;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import petstone.project.animalisland.R;
 
 public class MypageInfoEditActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mypage_info_edit);
+        setContentView(R.layout.mypage_info_edit);// 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
     }
 }
