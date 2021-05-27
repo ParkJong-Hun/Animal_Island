72d6874ef05c9c9d87b8e96affada53fa2c5eef5
ParkJong-Hun <thesn515@gmail.com>
2021-05-27 17:45:47 +0900

    2021/05/27 17:45 공지사항, 이벤트

---
+++ app/src/main/java/petstone/project/animalisland/component/MypageNewsDialog.java
@@ -0,0 +1,41 @@
+package petstone.project.animalisland.component;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+import petstone.project.animalisland.R;
+
+public class MypageNewsDialog {
+    private Context context;
+    String article;
+
+    public MypageNewsDialog(Context context) {
+        this.context = context;
+    }
+
+    public void addArticle(String article) {
+        this.article = article;
+    }
+
+    public void show() {
+        final Dialog dlg = new Dialog(context);
+
+        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dlg.setContentView(R.layout.mypage_news_component);
+        dlg.show();
+        TextView article_tv = dlg.findViewById(R.id.mypage_news_article);
+        article_tv.setText(article);
+        Button close = dlg.findViewById(R.id.mypage_news_button);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dlg.dismiss();
+            }
+        });
+    }
+}
