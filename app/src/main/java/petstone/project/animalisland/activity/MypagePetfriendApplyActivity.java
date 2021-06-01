c0dd82a651870cffdc3574028da72ec64d49a0c2
ParkJong-Hun <thesn515@gmail.com>
2021-06-01 19:41:59 +0900

    2021/06/01 19:41 펫프렌즈 신청 토글 버튼

--- app/src/main/java/petstone/project/animalisland/activity/MypagePetfriendApplyActivity.java
+++ app/src/main/java/petstone/project/animalisland/activity/MypagePetfriendApplyActivity.java
@@ -3,7 +3,9 @@ package petstone.project.animalisland.activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,7 +14,8 @@ import petstone.project.animalisland.R;
 public class MypagePetfriendApplyActivity extends AppCompatActivity {
 
     Button cancel, submit;
-    ImageView back;
+    ImageView back, license1, license2, license3;
+    Switch toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,13 @@ public class MypagePetfriendApplyActivity extends AppCompatActivity {
         cancel = findViewById(R.id.mypage_petfriend_apply_cancel);
         submit = findViewById(R.id.mypage_petfriend_apply_submit);
         back = findViewById(R.id.back_mypage_petfriend_apply);
+        toggle = findViewById(R.id.mypage_petfriend_apply_toggleButton);
+        license1 = findViewById(R.id.mypage_petfriend_apply_license_image1);
+        license2 = findViewById(R.id.mypage_petfriend_apply_license_image2);
+        license3 = findViewById(R.id.mypage_petfriend_apply_license_image3);
+        license1.setVisibility(View.GONE);
+        license2.setVisibility(View.GONE);
+        license3.setVisibility(View.GONE);
 
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +45,22 @@ public class MypagePetfriendApplyActivity extends AppCompatActivity {
             public void onClick(View v) {
                 setResult(RESULT_OK);
                 finish();
+            }
+        });
+
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    license1.setVisibility(View.VISIBLE);
+                    license2.setVisibility(View.VISIBLE);
+                    license3.setVisibility(View.VISIBLE);
+                } else {
+                    license1.setVisibility(View.GONE);
+                    license2.setVisibility(View.GONE);
+                    license3.setVisibility(View.GONE);
+
+                }
             }
         });
 
