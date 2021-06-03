9159520a496c003a52ffc75ae73c186358e5e86b
ljaewook <72073790+ljaewook@users.noreply.github.com>
2021-06-03 11:04:32 +0900

    2021/06/03

--- app/src/main/java/petstone/project/animalisland/RegisterActivity.java
+++ app/src/main/java/petstone/project/animalisland/RegisterActivity.java
@@ -1,18 +1,32 @@
 package petstone.project.animalisland;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import petstone.project.animalisland.activity.MainActivity;
+
 public class RegisterActivity extends AppCompatActivity {
+    //선언부
+    Button btn_register;
+
+    //화면 초기화
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_create);
+
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
