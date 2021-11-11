380b06bc3097c6282a53f46974873a4afc0f2b88
ParkJong-Hun <thesn515@gmail.com>
2021-11-11 14:32:28 +0900

    2021/11/11 14:32 여러 질의문 비동기 null 문제 해결해야됨

--- app/src/main/java/petstone/project/animalisland/component/ChatListComponent.java
+++ app/src/main/java/petstone/project/animalisland/component/ChatListComponent.java
@@ -2,6 +2,7 @@ package petstone.project.animalisland.component;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +19,10 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 import petstone.project.animalisland.R;
 import petstone.project.animalisland.activity.ChatActivity;
@@ -30,88 +33,45 @@ public class ChatListComponent extends Fragment {
     ListView list;
     ChatAdapter listAdapter;
 
+    ArrayList<String> names = new ArrayList<String>();
+    ArrayList<String> updatedMessages = new ArrayList<String>();
+    ArrayList<Integer> updatedCounts = new ArrayList<Integer>();
+    ArrayList<Timestamp> updatedAts = new ArrayList<Timestamp>();
+    ArrayList<String> uids = new ArrayList<String>();
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.chatlist_component, container, false);
 
         list = rootView.findViewById(R.id.chat_lv1);
-        ArrayList<String> uids = new ArrayList<String>();
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        FirebaseAuth auth = FirebaseAuth.getInstance();
+        //TODO: 파이어베이스 로드 비동기 해결하기
+        loadData();
+        listAdapter = new ChatAdapter(getContext(), names, updatedMessages, updatedCounts, updatedAts);
+        list.setAdapter(listAdapter);
+        Log.d("d", names + "\n" + updatedMessages + "\n" + updatedCounts + "\n" + updatedAts + "\n");
 
-        db.collection("chats")
-                .whereEqualTo("uid", auth.getUid())
-                .get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                    @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        ArrayList<String> names = new ArrayList<String>();
-                        ArrayList<String> updatedMessages = new ArrayList<String>();
-                        ArrayList<Integer> updatedCounts = new ArrayList<Integer>();
-                        ArrayList<Timestamp> updatedAts = new ArrayList<Timestamp>();
-
-                        for (DocumentSnapshot document : queryDocumentSnapshots
-                             ) {
-                            if (!document.getString("uid").equals(auth.getUid()))
-                                db.collection("users")
-                                        .document(document.getString("uid"))
-                                        .get()
-                                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                                            @Override
-                                            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                                names.add(documentSnapshot.getString("nickname"));
-                                                uids.add(documentSnapshot.getString("uid"));
-                                            }
-                                        });
-                            else {
-                                db.collection("users")
-                                        .document(document.getString("uid2"))
-                                        .get()
-                                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                                            @Override
-                                            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                                names.add(documentSnapshot.getString("nickname"));
-                                                uids.add(documentSnapshot.getString("uid"));
-                                            }
-                                        });
-                            }
-                            document.getReference().collection("messages")
-                                    .get()
-                                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                                        @Override
-                                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                                            List<DocumentSnapshot> documents = queryDocumentSnapshots.getDocuments();
-                                            updatedMessages.add(documents.get(documents.size() - 1).getString("message"));
-                                            updatedAts.add(documents.get(documents.size() - 1).getTimestamp("date"));
-                                        }
-                                    });
-                            document.getReference().collection("messages")
-                                    .whereEqualTo("readed", 1)
-                                    .whereNotEqualTo("uid", auth.getUid())
-                                    .get()
-                                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                                        @Override
-                                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                                            updatedCounts.add(queryDocumentSnapshots.getDocuments().size());
-                                        }
-                                    });
-                        }
-                        //TODO: 아마도 데이터 동기화 안될듯
-                        listAdapter = new ChatAdapter(getContext(), names, updatedMessages, updatedCounts, updatedAts);
-                        list.setAdapter(listAdapter);
-                    }
-                });
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(getContext(), ChatActivity.class);
-                //TODO: 상대방 uid 보내기
                 intent.putExtra("whoUID", uids);
                 startActivity(intent);
             }
         });
 
         return rootView;
+    }
+
+    public interface Callback {
+        void success(String data);
+    }
+
+    public void loadData() {
+
     }
 }
